5527cc280f01830a4f12c77860a994aae3613e14
tofae <arnobtuhin,j@gmail.com>
2023-07-23 12:39:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// require("dotenv").config()
+// const express = require("express")
+// const port = process.env.PORT || 8888
+// const app = express()
+// const jwt = require("jsonwebtoken")
+// const cors = require("cors")
+// const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+
+require("dotenv").config()
+const express = require("express")
+const port = process.env.PORT || 5000
+const app = express()
+const cors = require("cors")
+
+
+
